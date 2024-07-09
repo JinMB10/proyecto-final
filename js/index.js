eb4aa382d9d74fc7ca1e2ff51c0832c017c4804d
Elsa-Cotrina <elsacotrina58@gmail.com>
2024-07-09 00:19:59 -0500

    se modifico contactanos

--- js/index.js
+++ js/index.js
@@ -7,8 +7,9 @@ document.addEventListener("DOMContentLoaded", function() {
     menuIcon.addEventListener("click", function() {
       menu.classList.toggle("active");
     });
-});
-
+    
+  });
+  
 document.querySelector('.parrafo').innerHTML = "Bean Scene es una cafetería que le ofrece café de calidad que le ayuda a aumentar suproductividad ymejorar su estado de ánimo. Tomar una taza de café es bueno, pero tomar una taza de café de verdad esmejor. No hay duda de que disfrutarás de este café más que otros que hayas probado alguna vez."
 
 document.querySelector('.btn').innerHTML = "Aprende más";
